2580f9ed743b2c671b13a3058e7f08f698d2201f
Amul Sinfal <amul.sinfal@gmail.com>
2025-07-27 17:17:26 +0530

    committed files.

--- src/main/java/listeners/RetryTests.java
+++ src/main/java/listeners/RetryTests.java
@@ -1,18 +1,26 @@
 package listeners;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
+import com.aventstack.extentreports.Status;
+
+import pages.LoginPage;
 import utilities.ConfigReader;
 
 public class RetryTests implements IRetryAnalyzer {
-
+	
+	private static final Logger log = LogManager.getLogger(RetryTests.class);
 	private static final int MaxRetryCount =  Integer.parseInt(ConfigReader.getValue("QA", "maxRetryCount"));
 	private static int retryCount = 1;
 
 	@Override
 	public boolean retry(ITestResult result) {
 		if (retryCount <= MaxRetryCount) {
+			log.warn("Retrying test: Attempt " + retryCount + " of " + MaxRetryCount);
+			ReportListeners.test.log(Status.WARNING, "Retrying failing test: Attempt " + retryCount + " of " + MaxRetryCount);
 			retryCount++;
 			return true;
 		}
